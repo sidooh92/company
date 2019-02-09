ccafc2394ca5886ece89c36b03288d6ed2ab7e28
robert <robert.piotrowski92@gmail.com>
2019-02-09 12:17:48 +0100

    actual project status - done task 1

--- src/main/java/com/rent/company/Main.java
+++ src/main/java/com/rent/company/Main.java
@@ -15,21 +15,42 @@ public class Main {
     //zad6. tworzenie samochodu
     //zad7. wypozyczenie samochodu...
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
         RentCompanyService rentCompanyService = new RentCompanyServiceImpl();
 
-        Scanner scanner = new Scanner(System.in);
+        System.out.println("How program works:");
+        System.out.println("Option 1 to create new company - pass all needed info");
+        System.out.println("Option 2 to create department for given company");
+        System.out.println("Option 3 display company with departments");
+        System.out.println("Option 4 exit program");
+
+        String action = "";
+        while (!action.equalsIgnoreCase("4")) {
+            action = scanner.nextLine();
+            if (action.equalsIgnoreCase("1")) {
+                RentCompany newCompany = createCompany(scanner, rentCompanyService);
+
+            } else if (action.equalsIgnoreCase("2")) {
+
+
+            } else if (action.equalsIgnoreCase("3")) {
+
+            }
+
+
+        }
+
+
+    }
+
+    private static RentCompany createCompany(Scanner scanner, RentCompanyService rentCompanyService) {
         String name = scanner.nextLine();
         String website = scanner.nextLine();
         String address = scanner.nextLine();
         String owner = scanner.nextLine();
         String logo = scanner.nextLine();
-        RentCompany newCompany = rentCompanyService.createNewCompany(name, website,
+        return rentCompanyService.createNewCompany(name, website,
                 address, owner, logo);
-
-        System.out.println(newCompany);
-
-
     }
 
 
