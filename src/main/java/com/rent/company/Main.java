f8f8f788fafbac2cb290b3b2632509837b6e2854
robert <robert.piotrowski92@gmail.com>
2019-02-09 11:37:08 +0100

    actual project status

--- src/main/java/com/rent/company/Main.java
+++ src/main/java/com/rent/company/Main.java
@@ -5,13 +5,17 @@ import com.rent.company.service.RentCompanyService;
 import com.rent.company.service.RentCompanyServiceImpl;
 
 public class Main {
-
+    //zad1. dodawanie wypozyczalni
+    //zad2. dodawanie i usuwanie oddzialow z wypozyczalni
+    //zad3. tworzenie pracownikow
+    //zad4. przypisywanie praconikow do oddzialu
+    //zad5. tworzenie klienta
+    //zad6. tworzenie samochodu
+    //zad7. wypozyczenie samochodu...
     public static void main(String[] args) {
 
 
         RentCompanyService rentCompanyService = new RentCompanyServiceImpl();
-        //zad1. dodawanie wypozyczalni
-        //zad2. dodawanie i usuwanie oddzialow z wypozyczalni
 
 
 
